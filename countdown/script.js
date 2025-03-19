d3266263cdb768a5018105515af2391828e18aa5
Murilo Sorbo <developermurilo@gmail.com>
2025-03-19 18:41:05 -0500

    Audio player

--- countdown/script.js
+++ countdown/script.js
@@ -20,6 +20,39 @@ function updateCountdown(targetDate, elementId, callback) {
     setInterval(calculateTime, 1000);
 }
 
+const musicList = [
+    "music/Escreve ai.mp3",
+];
+
+const audio = document.getElementById("audio");
+const title = document.getElementById("music-title");
+const progress = document.getElementById("progress");
+
+function getRandomMusic() {
+    return musicList[Math.floor(Math.random() * musicList.length)];
+}
+
+function playMusic(music) {
+    audio.src = music;
+
+    title.innerText = music.substring(6).split(".")[0];
+    audio.play();
+}
+
+function nextMusic() {
+    playMusic(getRandomMusic());
+}
+
+function updateProgress() {
+    const percentage = (audio.currentTime / audio.duration) * 100;
+    progress.style.width = percentage + "%";
+}
+
+// Toca uma música aleatória ao carregar
+window.onload = () => {
+    playMusic(getRandomMusic());
+};
+
 // Contagem regressiva para 8 de maio de 2025
 let may8_2025 = new Date("May 8, 2025 00:00:00").getTime();
 updateCountdown(may8_2025, "countdownHeader", () => {
